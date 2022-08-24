16236110166a698fddfbba8831e914d907ea58ab
poivronjaune <poivronjaune@gmail.com>
2022-08-24 16:30:42 -0400

    Start of test cases

---
+++ TESTS/test_config.py
@@ -0,0 +1,41 @@
+import os
+import pytest
+
+def test_if_LOG_folder_exists():
+    assert os.path.exists('LOG\\')
+
+# Is this necessary? Why not use a @mark.parametrize() or load requirements.txt and loop though
+def test_if_package_installed_yfinance():
+    try:
+        import yfinance
+        assert True
+    except:
+        assert False
+
+def test_if_package_installed_pandas():
+    try:
+        import pandas
+        assert True
+    except:
+        assert False
+
+def test_if_package_installed_numpy():
+    try:
+        import numpy
+        assert True
+    except:
+        assert False
+
+def test_if_package_installed_matplotlib():
+    try:
+        import matplotlib
+        assert True
+    except:
+        assert False
+
+def test_if_package_installed_ta():
+    try:
+        import ta
+        assert True
+    except:
+        assert False        

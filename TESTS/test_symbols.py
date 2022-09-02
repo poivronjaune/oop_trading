a1b349e7cd1e35183c20ffd6d01f455b27f795c6
poivronjaune <poivronjaune@gmail.com>
2022-09-02 17:10:14 -0400

    Convert STR to DATAFRAME

--- TESTS/test_symbols.py
+++ TESTS/test_symbols.py
@@ -1,5 +1,6 @@
 import pytest
 import pandas as pd
+from bs4 import BeautifulSoup, element
 from backtest_lib.symbols import Symbols
 
 
@@ -9,6 +10,8 @@ class TestSymbolsClass:
         assert instance is not None
         assert instance.html == None
         assert instance.symbols_df == None
+        assert instance.line_marker == "First Date:"
+        assert instance.source == "First Trade Data"
         # assert instance.start_date == Backtest.DEFAULT_START_DATE
         # assert instance.end_date == Backtest.DEFAULT_END_DATE
         # assert instance.data == None
@@ -18,21 +21,50 @@ class TestSymbolsClass:
         instance.get_html_data_from_firstratedata_web_site()
         assert instance.html is not None
 
-    def test_extract_symbols_from_web_page_NO_WEBPAGE(self):
+    def test_get_html_data_from_bad_web_site(self):
         instance = Symbols()
-        instance.extract_symbols_from_web_page()
+        instance.url = "bad url"
+        with pytest.raises(Exception):
+            instance.get_html_data_from_firstratedata_web_site()
+
+    def test_get_html_data_from_web_site_no_line_marker_found(self):
+        instance = Symbols()
+        instance.url = "https://en.wikipedia.org/wiki/Stock_market"
+        with pytest.raises(Exception):
+            instance.get_html_data_from_firstratedata_web_site()
+
+
+    def test_extract_symbol_lines_from_html_content_using_default_line_marker(self):
+        instance = Symbols()
+        instance.get_html_data_from_firstratedata_web_site()
+        symbols_list = instance.extract_symbol_lines_from_html_content()
+        assert symbols_list is not None
+        assert instance.line_marker in symbols_list[0]
+        
+    def test_no_html_returned_to_extract_from(self):
+        instance = Symbols()
+        instance.html = None
+        instance.extract_symbol_lines_from_html_content()
         assert instance.symbols_df is None
 
-    # def test_extract_symbols_from_web_page(self):
-    #     instance = Symbols()
-    #     instance.get_html_data_from_firstratedata_web_site()
-    #     instance.extract_symbols_from_web_page()
-    #     assert instance.symbols_df is not None
-    #     assert isinstance(instance.symbols_df, pd.core.frame.DataFrame)
-    #     assert len(instance.symbols_df) > 0
-
-    def test_convert_strings_list_to_dataframe(self):
+    def test_no_symbols_found_in_html_page(self):
         instance = Symbols()
-        str1 = ['AA (Alcoa Corporation) First Date:18-Oct-2016 -> Last Date:31-Aug-2022']
-        result = instance.__convert_strings_list_to_dataframe(str1)
-        assert result is not None
+        instance.get_html_data_from_firstratedata_web_site()
+        instance.line_marker = "Bad marker"
+        instance.extract_symbol_lines_from_html_content()
+        assert instance.symbols_df is None
+
+
+    def test_convert_symbol_lines_to_dataframe_bad_list(self):
+        instance = Symbols()
+        symbol_lines = "not a list type"
+        instance.convert_symbol_lines_to_dataframe(symbol_lines)
+        assert instance.symbols_df is None
+
+    def test_convert_symbol_lines_to_dataframe(self):
+        instance = Symbols()
+        instance.get_html_data_from_firstratedata_web_site()
+        symbol_lines = instance.extract_symbol_lines_from_html_content()
+        instance.convert_symbol_lines_to_dataframe(symbol_lines)
+        assert isinstance(instance.symbols_df, pd.DataFrame)
+        assert len(instance.symbols_df) > 0

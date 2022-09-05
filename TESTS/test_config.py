481fb293ff280c7448588e34c514c98f748a73ec
PoivronJaune <poivronjaune@gmail.com>
2022-09-05 16:49:12 -0400

    Symbols functions

---
+++ TESTS/test_config.py
@@ -0,0 +1,18 @@
+import os
+import pytest
+
+def test_if_LOG_folder_exists():
+    assert os.path.exists("log\\")
+
+
+def test_if_all_packages_are_installed():
+    # If package missing test run will not reach assert statement
+    import yfinance
+    import pandas
+    import numpy
+    import matplotlib
+    import ta
+    import bs4
+    import sqlalchemy
+
+    assert True    
